d8ce35e40a6913e357d611313ea8ba2bf5c0ab28
xuanphuda <xuanphuda@gmail.com>
2026-03-03 21:54:14 +0700

    refactor: refactor folder structure

--- backend/flavortales-backend/flavortales-common/src/main/java/com/flavortales/common/exception/AccountAlreadyVerifiedException.java
+++
@@ -1,7 +0,0 @@
-package com.flavortales.common.exception;
-
-public class AccountAlreadyVerifiedException extends RuntimeException {
-    public AccountAlreadyVerifiedException() {
-        super("Account is already verified");
-    }
-}

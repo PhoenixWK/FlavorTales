a9814b162d673ffe5f554f1fb08f42387c259d57
xuanphuda <xuanphuda@gmail.com>
2026-03-04 19:57:07 +0700

    refactor: remove backend/ and frontend/ subfolders (now referenced directly from sibling folders)

--- backend/flavortales-common/src/main/java/com/flavortales/common/exception/ResendLimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.flavortales.common.exception;
-
-public class ResendLimitExceededException extends RuntimeException {
-    public ResendLimitExceededException() {
-        super("Resend limit reached. Maximum 3 resends are allowed per account.");
-    }
-}
--- backend/flavortales-user/src/main/java/com/flavortales/user/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.flavortales.user.entity;
-
-public enum Role {
-    vendor,
-    admin
-}

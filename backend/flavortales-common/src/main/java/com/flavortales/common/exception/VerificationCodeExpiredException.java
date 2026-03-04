5191b18cceac56b624224cd9753e0c9579e51fba
xuanphuda <xuanphuda@gmail.com>
2026-03-04 15:56:32 +0700

    merge: sync backend branch changes into backend/ folder

--- backend/flavortales-common/src/main/java/com/flavortales/common/exception/VerificationCodeExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.flavortales.common.exception;
-
-public class VerificationCodeExpiredException extends RuntimeException {
-    public VerificationCodeExpiredException() {
-        super("Verification code has expired");
-    }
-}
